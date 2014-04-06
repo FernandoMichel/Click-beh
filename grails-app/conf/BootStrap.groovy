710f124bcba8ac64ef5d6e7e09eadcfd4868ea9c
Daniel <Daniel@Daniel_Michel>
2014-04-06 12:53:06 -0500

    agregadas instancias de prueba para Profesor, Alumno e Inscripcion en el Bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,43 @@
+import escuelaingles.*
 class BootStrap {
 
     def init = { servletContext ->
+        def profePrueba=new Profesor(
+        nombre:"Profe",
+        apellp:"de",
+        apellm:"prueba",
+        contrasena:"pass",
+        correo:"daniel_michel@ciencias.unam.mx",
+        nivel:"Principiante",
+        horario:"matutino",
+        aceptado:true
+        )
+        profePrueba.save()
+        if(profePrueba.hasErrors()){
+            println profePrueba.errors
+        }
+        def alumnoPrueba=new Alumno(
+        nombre:"Alumno",
+        apellp:"de",
+        apellm:"prueba",
+        contrasena:"pass",
+        correo:"fernando_michel@ciencias.unam.mx",
+        telefono:"telefono"
+        )
+        alumnoPrueba.save()
+        if(alumnoPrueba.hasErrors()){
+            println alumnoPrueba.errors
+        }
+        def inscPrueba=new Inscripcion(
+            alumno: alumnoPrueba,
+            profesor: profePrueba,
+            aceptado: false
+        )
+        inscPrueba.save()
+        if(inscPrueba.hasErrors()){
+            println inscPrueba.errors
+        }
+
     }
     def destroy = {
     }
